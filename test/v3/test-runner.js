b75a1644912cba3d81b934dc3b2e2e1113f8d410
shturec <sthrakal@gmail.com>
2017-10-18 14:57:25 +0300

    switched to test runner rather than strictly service

--- test/v3/test-runner.js
+++ test/v3/test-runner.js
@@ -25,33 +25,43 @@ var endResponse = function(){
 	response.close();
 };	
 
-exports.service = function(settings) {
+exports.run = function(settings) {
 
-	var httpReqHeaderAccept = request.getHeader("Accept").replace(/\\/g,'').split(',');		
-	response.setContentType("application/json; charset=UTF-8");
-	response.setCharacterEncoding("UTF-8");
+	if(!settings.execute || (typeof settings.execute !== 'function')){
+		throw Error('[Test runner]  Invallid configuration: missing execute function');
+	}
 	
-	if(findInAcceptHeader(httpReqHeaderAccept, 'text/html')){
-		console.info('[Test Runner] Handling request for HTML test run results');
-		
-		var requestUrl = URI(request.getRequestURL()).normalizePath().path();
-		console.info('[Test Runner] redirecting to /services/v3/web/test/v3/ui/tests_dashboard.html with URL query string rewrite to url='+requestUrl);
-		response.addHeader('Location', '/services/v3/web/test/v3/ui/tests_dashboard.html?url='+requestUrl);
-		response.setStatus(response.FOUND);
-		
-	} else if (findInAcceptHeader(httpReqHeaderAccept, 'application/xml') || findInAcceptHeader(httpReqHeaderAccept, 'text/xml') || findInAcceptHeader(httpReqHeaderAccept, '*/xml')){
-		console.info('[Test Runner] Handling request for JSON test run results');
+	if(settings.serviceReporter) {
 
-		settings.serviceReporter.forMedia("xml");	
+		var httpReqHeaderAccept = request.getHeader("Accept").replace(/\\/g,'').split(',');		
+		response.setContentType("application/json; charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		if(findInAcceptHeader(httpReqHeaderAccept, 'text/html')){
+			console.info('[Test Runner] Handling request for HTML test run results');
+			
+			var requestUrl = URI(request.getRequestURL()).normalizePath().path();
+			console.info('[Test Runner] redirecting to /services/v3/web/test/v3/ui/tests_dashboard.html with URL query string rewrite to url='+requestUrl);
+			response.addHeader('Location', '/services/v3/web/test/v3/ui/tests_dashboard.html?url='+requestUrl);
+			response.setStatus(response.FOUND);
+			
+		} else if (findInAcceptHeader(httpReqHeaderAccept, 'application/xml') || findInAcceptHeader(httpReqHeaderAccept, 'text/xml') || findInAcceptHeader(httpReqHeaderAccept, '*/xml')){
+			console.info('[Test Runner] Handling request for JSON test run results');
+
+			settings.serviceReporter.forMedia("xml");	
+			settings.execute();
+			
+		} else if (findInAcceptHeader(httpReqHeaderAccept, 'application/json') || findInAcceptHeader(httpReqHeaderAccept, 'text/json') || findInAcceptHeader(httpReqHeaderAccept, '*/json') || findInAcceptHeader(httpReqHeaderAccept, '*/*')){
+			console.info('[Test Runner] Handling request for JSON test run results');
+
+			settings.serviceReporter.forMedia("json");	
+			settings.execute();
+		} else {
+			// send BadRequest
+		}
+
+		endResponse();
+	} else {
 		settings.execute();
-		
-	}	else if (findInAcceptHeader(httpReqHeaderAccept, 'application/json') || findInAcceptHeader(httpReqHeaderAccept, 'text/json') || findInAcceptHeader(httpReqHeaderAccept, '*/json') || findInAcceptHeader(httpReqHeaderAccept, '*/*')){
-		console.info('[Test Runner] Handling request for JSON test run results');
-
-		settings.serviceReporter.forMedia("json");	
-		settings.execute();
-	}	
-
-	endResponse();
-	
+	}
 };
