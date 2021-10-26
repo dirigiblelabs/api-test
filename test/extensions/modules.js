d64f49020d50b6dc6712bf1117a9784e85a4b9ed
Ivan <iwolkkov@gmail.com>
2021-10-26 10:13:29 +0300

    Added d.ts for module.

--- test/extensions/modules.js
+++ test/extensions/modules.js
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * Contributors:
- *   SAP - initial API and implementation
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
  */
-exports.getContent = function() {
-	return [{
-		name: "test/v4/test-runner",
-		description: "Test Runner API"
-	}];
+var registry = require("platform/v4/registry");
+exports.getContent = function () {
+    var file = registry.getText("test/extensions/modules.json")
+    return JSON.parse(file);
 };
